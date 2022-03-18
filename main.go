e6e2dd927fd263ecc2717470ed51538cbc3e6ba8
flabiohs <flabiohs@gmail.com>
2022-03-18 17:33:56 -0500

    change

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/flabio/Infeastructure/routers"
 )
 
 func main() {
-	rd := os.Getenv("MYSQL_HOST")
-	log.Println(rd)
 	routers.NewRouter()
 
 }
